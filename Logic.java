b8f4ec8d8c305e8c061f23b2d685e604a0910b90
ZhukDmitriy350504 <dmitriy_zhuk_95@mail.ru>
2015-12-02 00:53:09 +0300

    Added check for mistakes

--- Logic.java
+++ Logic.java
@@ -2,41 +2,11 @@ package sample;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
 
 public class Logic{
-
-    public static boolean checkInternetConnection() {
-        Boolean result = false;
-        HttpURLConnection con = null;
-            try {
-                // HttpURLConnection.setFollowRedirects(false);
-                // HttpURLConnection.setInstanceFollowRedirects(false)
-                con = (HttpURLConnection) new URL("http://www.nepogoda.ru").openConnection();
-                con.setRequestMethod("HEAD");
-                result = (con.getResponseCode() == HttpURLConnection.HTTP_OK);
-                System.out.println("Connection OK!!");
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                if (con != null) {
-                    try {
-                        con.disconnect();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        System.out.println("NO CONECTION!!");
-                    }
-                }
-            }
-
-
-            return result;
-        }
-
-
     public static String SourceCode(String pageAddress, String codePage) throws Exception {
 
         StringBuilder sb = new StringBuilder();
